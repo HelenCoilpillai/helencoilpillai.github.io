ed6d419227a19d425960adf8409cf8f35dc98f5b
Helen Coilpillai <83344985+HelenCoilpillai@users.noreply.github.com>
2021-10-22 16:42:53 +0530

    Update 01_CodewarsKataLevel4.php

--- 01_CodewarsKataLevel4.php
+++ 01_CodewarsKataLevel4.php
@@ -1,78 +1,77 @@
 <?php
-function nextBigger($givenNumber) 
+function nextBigger($givenNumber)
 {
-   $lengthOfGivenNumber = strlen((string)$givenNumber);
-   $numberArray = str_split($givenNumber);  
- 
-         $indexToBeginSwap = getIndexToBeginSwap($lengthOfGivenNumber, $numberArray);    
-  
-  if ($indexToBeginSwap != SWAP_NOT_REQUIRED) {
-         $smallestNumberIndex = findTheSmallestDigitIndex ($indexToBeginSwap, $numberArray, $lengthOfGivenNumber);
-               
-  if (empty($smallestNumberIndex) == true) {
-         $numberArray = swapNumbers($numberArray, $indexToBeginSwap, $indexToBeginSwap - 1);
-  } else {
-         $numberArray = swapNumbers($numberArray, $indexToBeginSwap - 1, $smallestNumberIndex);
-         }         
-    
-         $numberArray = reorderPartOfArray($numberArray, $indexToBeginSwap);       
-         $rearrangedNumber = (int)implode("", $numberArray);
-     
-      return  $rearrangedNumber;            
+    $lengthOfGivenNumber = strlen((string)$givenNumber);
+    $numberArray = str_split($givenNumber);
+
+    $indexToBeginSwap = getIndexToBeginSwap($lengthOfGivenNumber, $numberArray);
+
+    if ($indexToBeginSwap != SWAP_NOT_REQUIRED) {
+        $smallestNumberIndex = findTheSmallestDigitIndex($indexToBeginSwap, $numberArray, $lengthOfGivenNumber);
+
+        if (empty($smallestNumberIndex) == true) {
+            $numberArray = swapNumbers($numberArray, $indexToBeginSwap, $indexToBeginSwap - 1);
+        } else {
+            $numberArray = swapNumbers($numberArray, $indexToBeginSwap - 1, $smallestNumberIndex);
+        }
+
+        $numberArray = reorderPartOfArray($numberArray, $indexToBeginSwap);
+        $rearrangedNumber = (int)implode("", $numberArray);
+
+        return $rearrangedNumber;
     }
 
-  return -1;
+    return -1;
 }
 
 const SWAP_NOT_REQUIRED = -1;
-function getIndexToBeginSwap ($lengthOfGivenNumber, $numberArray)
-{   
- for ($i = $lengthOfGivenNumber - 1; $i >= 0; $i--) {
-    
-    if (
-       array_key_exists($i - 1, $numberArray) == true 
-       && ($numberArray[$i]) > ($numberArray[$i - 1])
-    ) {       
-      return $i;
+function getIndexToBeginSwap($lengthOfGivenNumber, $numberArray)
+{
+    for ($i = $lengthOfGivenNumber - 1; $i >= 0; $i--) {
+
+        if (
+            array_key_exists($i - 1, $numberArray) == true
+            && ($numberArray[$i]) > ($numberArray[$i - 1])
+        ) {
+            return $i;
+        }
     }
-  }
-      return SWAP_NOT_REQUIRED;
+    return SWAP_NOT_REQUIRED;
 }
 
 function swapNumbers($numberArray, $numberIndex1ToSwap, $numberIndex2ToSwap)
-{  
-    $tempValue = $numberArray[$numberIndex1ToSwap];    
+{
+    $tempValue = $numberArray[$numberIndex1ToSwap];
     $numberArray[$numberIndex1ToSwap] = $numberArray[$numberIndex2ToSwap];
-    $numberArray[$numberIndex2ToSwap] = $tempValue; 
-    return $numberArray;  
+    $numberArray[$numberIndex2ToSwap] = $tempValue;
+    return $numberArray;
 }
 
-function findTheSmallestDigitIndex($arrayIndexPointToStartCheck, $numberArray, $lengthOfGivenNumber) {
-    $baseNumber = $numberArray[$arrayIndexPointToStartCheck - 1]; 
-    $smallestNumber = $numberArray[$arrayIndexPointToStartCheck];    
-    
-      for ($arrayIndexPointToStartCheck; $arrayIndexPointToStartCheck < $lengthOfGivenNumber; $arrayIndexPointToStartCheck++) {
-      
+function findTheSmallestDigitIndex($arrayIndexPointToStartCheck, $numberArray, $lengthOfGivenNumber)
+{
+    $baseNumber = $numberArray[$arrayIndexPointToStartCheck - 1];
+    $smallestNumber = $numberArray[$arrayIndexPointToStartCheck];
+
+    for ($arrayIndexPointToStartCheck; $arrayIndexPointToStartCheck < $lengthOfGivenNumber; $arrayIndexPointToStartCheck++) {
+
         if (
             array_key_exists($arrayIndexPointToStartCheck, $numberArray) === true
-            && $baseNumber < $numberArray[$arrayIndexPointToStartCheck] 
+            && $baseNumber < $numberArray[$arrayIndexPointToStartCheck]
             && $numberArray[$arrayIndexPointToStartCheck] <= $smallestNumber
-        ) {            
+        ) {
             $smallestNumberIndex = $arrayIndexPointToStartCheck;
         }
-        
-       }
-            return $smallestNumberIndex;
+
     }
+    return $smallestNumberIndex;
+}
 
 function reorderPartOfArray($numberArray, $indexToBeginSwap)
-  {
-  $reorderedPartOfNumber = array_splice($numberArray, $indexToBeginSwap);    
-  sort($reorderedPartOfNumber, SORT_NUMERIC);
-  
-  return $numberArray = array_merge($numberArray, $reorderedPartOfNumber);
-  }
-?>
+{
+    $reorderedPartOfNumber = array_splice($numberArray, $indexToBeginSwap);
+    sort($reorderedPartOfNumber, SORT_NUMERIC);
 
-// Link to Kata: https://www.codewars.com/kata/55983863da40caa2c900004e
-// Link to answer : https://www.codewars.com/kata/reviews/5eb073560af08a000188bd80/groups/614d7044230b900001a03a9e
+    return $numberArray = array_merge($numberArray, $reorderedPartOfNumber);
+}
+
+?>
