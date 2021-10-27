cdc9c1d29c42f2fcd224d330b2b9609f165f1dc2
Helen Coilpillai <83344985+HelenCoilpillai@users.noreply.github.com>
2021-10-27 11:15:23 +0530

    Update init.php

--- init.php
+++ init.php
@@ -1,7 +1,6 @@
 <?php
 
-include_once("methods/wideMouthFrog.php");
-include_once("challengers/theWideMouthFrog.php");
+include_once("methods/wideMouthFrog.php");;
 include_once("methods/errors.php");
 
 ?>
