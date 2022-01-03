ee73bd863de0df9e8f69efad065bcd93eee51098
Helen <helen.coilpillai@visma.com>
2022-01-03 12:35:07 +0530

    The Wide Mouth Frog changes

--- app/Http/Controllers/TheWideMouthFrogController.php
+++ app/Http/Controllers/TheWideMouthFrogController.php
@@ -3,16 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\TheWideMouthFrogRequest;
-use App\Service\kata\TheWideMouthFrogService;
+use App\Service\Kata\TheWideMouthFrogService;
 
 class TheWideMouthFrogController extends Controller
 {
     public function animalNameFormSubmit(TheWideMouthFrogRequest $request)
     {
-        $animal = new TheWideMouthFrogService();
+        $wideMouthFrogService = new TheWideMouthFrogService();
 
         $animalName = $request->input('animal');
-        $mouthSize = $animal->getMouthSize($animalName);
-        return redirect()->back()->with('message', "The {$animalName}s mouth is {$mouthSize}!");
+        $mouthSize = $wideMouthFrogService->getMouthSize($animalName);
+        return redirect()->back()->with('message', "The {$animalName}'s mouth is {$mouthSize}!");
     }
 }
--- app/Service/Kata/TheWideMouthFrogService.php
+++ app/Service/Kata/TheWideMouthFrogService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Service\kata;
+namespace App\Service\Kata;
 
 class TheWideMouthFrogService
 {
