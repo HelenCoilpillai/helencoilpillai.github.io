fcd5e4f0bf6a98c3bd643850f07c0cfe46b81d99
Helen <helen.coilpillai@visma.com>
2022-01-07 16:53:52 +0530

    changes to head file, VerifyCsrfToken.php

--- resources/views/layout/components/header.blade.php
+++ resources/views/layout/components/header.blade.php
@@ -13,3 +13,11 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </div>
+
+<script>
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+</script>
