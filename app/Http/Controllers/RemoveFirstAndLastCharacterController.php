ba971bf8c842b0026bfad0def267d1ae2e683a30
Helen <helen.coilpillai@visma.com>
2022-02-23 15:21:39 +0530

    feat: kata8 - remove first and last character

--- app/Http/Controllers/RemoveFirstAndLastCharacterController.php
+++ app/Http/Controllers/RemoveFirstAndLastCharacterController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Requests\RemoveFirstAndLastCharacterRequest;
 use App\Service\Kata\RemoveFirstAndLastCharacterService;
 use Illuminate\Http\RedirectResponse;
-use phpDocumentor\Reflection\Types\Null_;
+use Illuminate\Routing\Redirector;
+use Illuminate\Support\Facades\Redirect;
 
 class RemoveFirstAndLastCharacterController extends Controller
 {
@@ -27,8 +28,19 @@ class RemoveFirstAndLastCharacterController extends Controller
     public function removeFirstAndLastCharacterFormSubmit(RemoveFirstAndLastCharacterRequest $request): RedirectResponse
     {
         $inputString = $request->input('input');
+
         $firstAndLastCharacterRemovedString = $this->removeFirstAndLastCharacterService->removeFirstAndLastCharacter($inputString);
-        return redirect()->back()
+
+        return $this->getRedirectObject()
+            ->back()
             ->with('message', "The first & last characters have been removed: {$firstAndLastCharacterRemovedString}");
+    }
+
+    /**
+     * @return Redirector
+     */
+    protected function getRedirectObject(): Redirector
+    {
+        return redirect();
     }
 }
---
+++ tests/Unit/ServiceTest/RemoveFirstAndLastCharacterServiceTest.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @author      Helen Coilpillai
+ * @since       PHP 7.4
+ * @copyright   ProActive 2022
+ *
+ * @run: php artisan test --filter RemoveFirstAndLastCharacterServiceTest
+ */
+
+namespace tests\Unit\ServiceTest;
+
+use App\Service\Kata\RemoveFirstAndLastCharacterService;
+use Mockery;
+use Mockery\MockInterface;
+use PHPUnit\Framework\TestCase;
+
+class RemoveFirstAndLastCharacterServiceTest extends TestCase
+{
+
+    public function setUp(): void
+    {
+
+    }
+
+    public function tearDown(): void
+    {
+        Mockery::close();
+
+        if (app() instanceof MockInterface) {
+            \Illuminate\Container\Container::setInstance(null);
+        }
+    }
+
+    /**
+     * @covers RemoveFirstAndLastCharacterService::removeFirstAndLastCharacter
+     */
+    public function testRemoveFirstAndLastCharacterEqualsGivenValueWhenTextLengthIsGreaterThanOne(): void
+    {
+        $removeFirstAndLastCharacterService = new RemoveFirstAndLastCharacterService();
+
+        $this->assertEquals('nit Testin',
+            $removeFirstAndLastCharacterService->removeFirstAndLastCharacter('Unit Testing'));
+    }
+
+    /**
+     * @covers RemoveFirstAndLastCharacterService::removeFirstAndLastCharacter
+     */
+    public function testRemoveFirstAndLastCharacterReturnsEmptyWhenTextLengthIsLessThanOne(): void
+    {
+        $removeFirstAndLastCharacterService = new RemoveFirstAndLastCharacterService();
+
+        $this->assertEmpty($removeFirstAndLastCharacterService->removeFirstAndLastCharacter('U'));
+    }
+}
